39cf5421dc3d15a939301245f3a8d325101c9577
Your Namemauler2007 <mauler2007@gmail.com>
2021-02-05 16:25:31 +0200

    change structure

--- app/js/main.js
+++ app/js/main.js
@@ -1,49 +1,47 @@
 $(function () {
-  
+
   $('.main-slider__inner').slick({
     fade: true,
-    arrows:false,
-    dots:true
-    // autoplay:true
+    arrows: false,
+    dots: true,
+    autoplay: true
   });
+
+
 
   // $('.product-card__btn-heart').on('click', function(e){
   //   e.preventDefault();
   //   $(this).toggleClass('product-card__btn-heart--active');
   // });
 
+
+  $(window).scroll(function () {
+    var height = $(window).scrollTop();
+
+    /*Если сделали скролл на 100px задаём новый класс для header*/
+    if (height > 100) {
+      $('header').addClass('header-fixed');
+    } else {
+      /*Если меньше 100px удаляем класс для header*/
+      $('header').removeClass('header-fixed');
+    }
+
+  });
+
   var mixer1 = mixitup('#MixItUp1', {
     selectors: {
       control: '.week__btn'
     }
   });
-  
+
   var mixer2 = mixitup('#MixItUp2', {
-      selectors: {
-        control: '.new__btn'
-      }
+    selectors: {
+      control: '.new__btn'
+    }
   });
-  
-  //  $('#MixItUp1').mixItUp({
-  //   selectors: {
-  //     control: '.week__btn'
-  //   }
-  // });
 
-  // $('#MixItUp2').mixItUp({
-  //   selectors: {
-  //     control: '.new__btn'
-  //   }
-  // });
-
-  // var mixer = mixitup('.week__box');
-
-
-  
   $('[data-fancybox]').fancybox({
     loop: true
   });
 
 });
-
-
